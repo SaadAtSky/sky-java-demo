343a28d69e682c4bccfa273d2afef01bdc0fe07d
believer4U- <saady6nov@gmail.com>
2023-07-24 13:47:34 +0100

    creates all classes, eat/grow method, ToString/equal methods

--- src/com/sky/creatingClasses/Animal.java
+++ src/com/sky/creatingClasses/Animal.java
@@ -3,6 +3,8 @@ package com.sky.creatingClasses;
 public class Animal {
     private int weight;
     private int height;
+    private int age;
+    private double satiety;
 
     public int getHeight() {
         return height;
@@ -28,5 +30,53 @@ public class Animal {
         this.weight = weight;
     }
 
-    private int age;
+    public void grow(){
+        height += 5;
+        age += 1;
+    }
+
+    public double getSatiety() {
+        return satiety;
+    }
+
+    public void setSatiety(double satiety) {
+        this.satiety = satiety;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal [age = "+this.getAge()+", height = "+this.getHeight()+", weight = "+this.getHeight()+", satiety = "+this.getSatiety()+"]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Animal animal = (Animal) o;
+
+        if (weight != animal.weight) return false;
+        if (height != animal.height) return false;
+        if (age != animal.age) return false;
+        return Double.compare(animal.satiety, satiety) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = weight;
+        result = 31 * result + height;
+        result = 31 * result + age;
+        temp = Double.doubleToLongBits(satiety);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    public void eat(){
+        satiety += 50;
+        if(satiety > 100){
+            satiety = 100;
+        }
+    }
 }
---
+++ src/com/sky/creatingClasses/Main.java
@@ -0,0 +1,7 @@
+package com.sky.creatingClasses;
+
+public class Main {
+    public static void main(String[] a){
+
+    }
+}
