d2e310e273df9d0b00330274226fc4d47bdf8c11
FergusTSmith <FergusTSmith@Gmail.com>
2023-07-26 14:32:07 +0100

    Made static

--- src/com/sky/inputs/Person.java
+++ src/com/sky/inputs/Person.java
@@ -8,43 +8,54 @@ public class Person {
     private String name;
     private Profession profession;
 
-    public Person(){};
-    public void personForm(){
+    public Person(int age, String name, Profession profession){
+        this.age = age;
+        this.name = name;
+        this.profession = profession;
+    };
+    public static Person personForm(){
         Scanner sc = new Scanner(System.in);
-        try{
-            System.out.println("Enter you age");
-            age = sc.nextInt();
-            sc.nextLine();
+        boolean detailsEntered = false;
+        while(!detailsEntered){
+            try{
+                System.out.println("Enter you age");
+                int enteredAge = sc.nextInt();
+                sc.nextLine();
 
-            System.out.println("Enter you name");
-            name = sc.nextLine();
+                System.out.println("Enter you name");
+                String enteredName = sc.nextLine();
 
-            System.out.println("Choose the correct option for profession: \n1) Software Dev \n2) SRE \n3) DevOps Engineer");
-            int option = sc.nextInt();
-            switch (option){
-                case 1:
-                    profession = Profession.SoftwareDeveloper;
-                    break;
-                case 2:
-                    profession = Profession.SRE;
-                    break;
-                case 3:
-                    profession = Profession.DevOpsEngineer;
-                    break;
+                System.out.println("Choose the correct option for profession: \n1) Software Dev \n2) SRE \n3) DevOps Engineer");
+                Profession professionEntered;
+                int option = sc.nextInt();
+                switch (option){
+                    case 1:
+                        professionEntered = Profession.SoftwareDeveloper;
+                        break;
+                    case 2:
+                        professionEntered = Profession.SRE;
+                        break;
+                    case 3:
+                        professionEntered = Profession.DevOpsEngineer;
+                        break;
+                }
+                detailsEntered = true;
+                return new Person(enteredAge, enteredName, professionEntered);
+            }
+            catch (ArithmeticException e){
+                System.out.println(e.getMessage());
+            }
+            catch (InputMismatchException e){
+                System.out.println("You entered the wrong input buddy");
+            }
+            catch (Exception e){
+                System.out.println("Don't really know");
+            }
+            finally {
+                System.out.println("Thank you for your time. Here are your entered details \n {age: "+age+", name: "+name+",profession: "+profession+"}");
             }
         }
-        catch (ArithmeticException e){
-            System.out.println(e.getMessage());
-        }
-        catch (InputMismatchException e){
-            System.out.println("You entered the wrong input buddy");
-        }
-        catch (Exception e){
-            System.out.println("Don't really know");
-        }
-        finally {
-            System.out.println("Thank you for your time. Here are your entered details \n {age: "+age+", name: "+name+",profession: "+profession+"}");
-        }
+
     }
 
     @Override
