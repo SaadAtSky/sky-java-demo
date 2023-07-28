b7a5d4648578e79b812a2401ad1ccb8e771c44ae
believer4U- <saady6nov@gmail.com>
2023-07-28 18:06:47 +0100

    everything except checking winner

--- src/com/sky/ticTacToe/Board.java
+++ src/com/sky/ticTacToe/Board.java
@@ -1,11 +1,21 @@
 package com.sky.ticTacToe;
 
+import java.util.Scanner;
+
 public class Board {
     final int rows = 3;
     final int cols = 3;
     private String[][] positions;
+
+    private boolean win;
+
+    public void setWin(boolean win) {
+        this.win = win;
+    }
+
     public Board() {
         this.positions = new String[rows][cols];
+        win = false;
     }
     public void initBoard(){
         for(int i = 0;i < cols;i++){
@@ -23,14 +33,32 @@ public class Board {
         }
     }
     public void move(Player p){
-
+        Scanner sc = new Scanner(System.in);
+        int[] move = new int[2];
+        do{
+            try{
+                System.out.println("Enter a valid move - 'row-enter-column-enter' ");
+                move[0] = sc.nextInt();
+                move[1] = sc.nextInt();
+            }catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }while (!checkValidMove(move));
+        positions[move[0]][move[1]] = p.getSymbol();
     }
-    public boolean checkValidMove(){
-
+    public boolean checkValidMove(int[] move){
+        if(positions[move[0]][move[1]].equals("-"))return true;
+        return false;
+    }
+    public boolean boardFull(){
+        for(int i = 0;i < cols;i++){
+            for(int j = 0;j < rows;j++){
+                if(positions[i][j] == "-")return false;
+            }
+        }
         return true;
     }
-    public boolean checkWin(){
-
-        return true;
+    public boolean isWin() {
+        return win;
     }
 }
--- src/com/sky/ticTacToe/Main.java
+++ src/com/sky/ticTacToe/Main.java
@@ -1,9 +1,47 @@
 package com.sky.ticTacToe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Board ticTacToe = new Board();
         ticTacToe.initBoard();
-        ticTacToe.displayBoard();
+
+        Scanner sc = new Scanner(System.in);
+        String name, symbol;
+
+        System.out.println("Enter player 1 name: ");
+        name = sc.nextLine();
+        System.out.println("Enter player 1 symbol: ");
+        symbol = sc.nextLine();
+        Player p1 = new Player(name,symbol);
+
+        System.out.println("Enter player 2 name: ");
+        name = sc.nextLine();
+        System.out.println("Enter player 2 symbol: ");
+        symbol = sc.nextLine();
+        Player p2 = new Player(name,symbol);
+
+        while(!ticTacToe.boardFull() || !ticTacToe.isWin()){
+            // Player 1
+            ticTacToe.displayBoard();
+            ticTacToe.move(p1);
+            if(ticTacToe.isWin()){
+                p1.incrementWins();
+                break;
+            }
+            if(ticTacToe.boardFull())break;
+
+            // Player 2
+            ticTacToe.displayBoard();
+            ticTacToe.move(p2);
+            if(ticTacToe.isWin()){
+                p2.incrementWins();
+                break;
+            }
+        }
+        System.out.println(p1.getName() + ": " + p1.getNoOfWins() +" "+ p2.getName() + ": " + p2.getNoOfWins());
+
+
     }
 }
